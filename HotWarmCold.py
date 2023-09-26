11c3414895ac9909c4ac91e72031d72ec86da2b0
Andras-Karczub <146126204+Andras-Karczub@users.noreply.github.com>
2023-09-26 23:03:39 +0200

    Update HotWarmCold.py

--- HotWarmCold.py
+++ HotWarmCold.py
@@ -6,8 +6,6 @@ maxguess = 10
 digits = 3
 
 def hotwarmcold():
-    print('Szia Bazsi')
-    print('Szép estét')
     GoalNum = ''
     numbers = list('0123456789')    #these digits can be choosen
     random.shuffle(numbers)         #shuffled into random order
